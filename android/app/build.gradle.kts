4b59df1a2156d915baa9621975f1c839fbf782c4
AndyLeo10 <alamoguizadoandresleonardo@gmail.com>
2025-03-27 09:57:25 -0500

    Fix ndkVersion in build.gradle.kts to resolve build error

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,8 @@ plugins {
 android {
     namespace = "com.example.eisenshower_todo"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    // ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973" // for solving an error
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
